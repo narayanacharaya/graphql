6defba659325f902eeced59cf0325c08c97bfe21
Narayan Acharya <acharayanarayan01@gmail.com>
2024-12-05 09:15:44 +0530

    removing chatgateyway form module also

--- src/sockets/sockets.module.ts
+++ src/sockets/sockets.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppGateway } from './gateways/app.gateway';
-import { ChatGateway } from './gateways/chat.gateway';
 import { NotificationService } from './services/notification.service';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  providers: [AppGateway, ChatGateway, NotificationService],
+  providers: [AppGateway, NotificationService],
   exports: [AppGateway],
 })
 export class SocketsModule {}
